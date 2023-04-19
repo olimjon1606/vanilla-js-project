898f61273bb10453b784f333df560f2a6dd77a92
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-04-19 16:43:05 +0200

    mistake

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 function addTodo(event) {
     event.preventDefault();
 
@@ -10,7 +13,7 @@ function addTodo(event) {
     todoDiv.classList.add("todo");
 
     const newTodo = document.createElement("li");
-    newTodo.innerText=todoInput.value;
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
 
@@ -25,5 +28,42 @@ function addTodo(event) {
     todoDiv.appendChild(trashButton);
 
     todoList.appendChild(todoDiv);
+    todoInput.value = ""
 
 }
+
+
+function deleteCheck(e) {
+    const item = e.target;
+
+    if (item.classList[0] === "trash-btn") {
+        const todo = item.parentElement;
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function () {
+            todo.remove()
+        })
+    }
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+
+        todo.classList.toggle("completed");
+    }
+
+}
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function (todo) {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+        }
+    });
+}
+
